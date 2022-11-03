6ea34debe5cac6c9dc43c1cec73b08f4a6f96638
pdimon54 <pdimon54@gmail.com>
2022-11-03 17:10:51 +0200

    task two impl

--- src/main/java/org/example/CheckNumberThread.java
+++ src/main/java/org/example/CheckNumberThread.java
@@ -1,13 +1,64 @@
 package org.example;
 
-public class CheckNumberThread extends Thread{
-    @Override
-    public void run() {
-        new Thread(() -> System.out.println()).start();
+import java.util.function.IntConsumer;
+
+public class CheckNumberThread {
+    private final int n;
+    private int currentNumber = 1;
+
+    public int getCurrentNumber() {
+        return currentNumber;
     }
 
-    private static void fizzbuzz(){
-
+    public CheckNumberThread(int n) {
+        this.n = n;
     }
 
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 != 0 || currentNumber % 5 == 0) {
+                wait();
+                continue;
+            }
+            printFizz.run();
+            currentNumber += 1;
+            notifyAll();
+        }
+    }
+
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 5 != 0 || currentNumber % 3 == 0) {
+                wait();
+                continue;
+            }
+            printBuzz.run();
+            currentNumber += 1;
+            notifyAll();
+        }
+    }
+
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 15 != 0) {
+                wait();
+                continue;
+            }
+            printFizzBuzz.run();
+            currentNumber += 1;
+            notifyAll();
+        }
+    }
+
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 == 0 || currentNumber % 5 == 0) {
+                wait();
+                continue;
+            }
+            printNumber.accept(currentNumber);
+            currentNumber += 1;
+            notifyAll();
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.function.IntConsumer;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -14,25 +15,45 @@ public class Main {
         SomeTimeSleepsThread.printCurrentSecondCount(t2);
         //////////////////////////////////////////////////////////////////////
         //////////////////////////////////////////////////////////////////////
+
+
         System.out.println("Second task:");
 
+        final int initialNumber = 15;
+        CheckNumberThread checkNumberThread = new CheckNumberThread(initialNumber);
+        Thread A = new Thread(() -> {
+            try {
+                checkNumberThread.fizz(()->System.out.println("fizz"));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread B = new Thread(() -> {
+            try {
+                checkNumberThread.buzz(()->System.out.println("buzz"));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread C = new Thread(() -> {
+            try {
+                checkNumberThread.fizzbuzz(()-> System.out.println("fizzbuzz"));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread D = new Thread(() -> {
+            try {
+                checkNumberThread.number(
+                    value -> System.out.println(checkNumberThread.getCurrentNumber()));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-
-//        final int initialNumber = 15;
-//
-//        Thread A = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                if (initialNumber%3 == 0 &&initialNumber%5 == 0){
-//                    System.out.println("fizzbuzz");
-//                }
-//            }
-//        });
-//
-//        for (int i = 1; i <= initialNumber; i++) {
-//            new Thread(() -> ().start();
-//        }
-
-
+        A.start();
+        B.start();
+        C.start();
+        D.start();
     }
 }
